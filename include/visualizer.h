9b844fcee2995373adc63dbcbe40bed00c248158
aya-matmata <aya.matmata@polytechnique.edu>
2025-06-04 19:05:52 +0200

    Visualization update and working code for now

--- include/visualizer.h
+++ include/visualizer.h
@@ -1,18 +1,21 @@
 #pragma once
+#include "body.h"
 #include <gtkmm.h>
-// #include "body.h"
 #include <vector>
-#include <mutex>
+
 
 class Visualizer : public Gtk::Window {
 public:
-    Visualizer(std::vector<Body>* bodies, std::mutex* mutex);
+    Visualizer(const double* all_positions, int n_bodies, int steps);
 
 protected:
+    Gtk::DrawingArea drawing_area;
+    const double* all_positions;
+    int n_bodies;
+    int total_frames;
+    int current_frame = 0;
+
     bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
     bool on_timeout();
-
-    Gtk::DrawingArea drawing_area;
-    std::vector<Body>* bodies;
-    std::mutex* mutex;
 };
+
