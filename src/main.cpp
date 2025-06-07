0c6f4836330e96f3aed2a161b28cb3081437cce8
Gabrio05 <73126191+Gabrio05@users.noreply.github.com>
2025-06-07 17:09:46 +0200

    Clean code slightly

--- src/main.cpp
+++ src/main.cpp
@@ -98,6 +98,11 @@ int main(int argc, char** argv)
     const std::vector<double> initial_body_velocities[n * 2]; // The velocities of bodies at time step t=0: {vx1, vy1, vx2, vy2, ...}
 
     // Allocate memory
+    // The matrix of forces between body i and j.
+    // forces[(i * n + j) * 2] for x, forces[(i * n + j) * 2 + 1] for y
+    // Additionally, forces[(i * n + j) * 2] == forces[(j * n + i) * 2] since matrix is symmetrical
+    // Stores position of all bodies for every time step
+    // all_positions[(i * n + j) * 2 (+1)] to access position_x of body j for timestep i (+1 for position_y)
     double* bis_forces = new double[n * n * 2];                         // Force matrix
     double* bis_all_positions = new double[n * total_time_steps * 2];   // Position buffer
     Body* bis_bodies = new Body[n];                                     // Array of Body instances
@@ -142,59 +147,3 @@ int main(int argc, char** argv)
     delete[] bodies;
     return 0;
 }
-
-/*int main()
-{
-    // Inputs:
-    const int threads = 16;                                   // The number of threads
-    const double step_time = 3600.0;                          // Time, in secs, in between each time step
-    const int total_time_steps = 1024;                        // The number of steps to simulate
-    const int n = 8;                                          // Number of bodies
-    const std::vector<double> initial_body_positions[n * 2];  // The position of bodies at time step t=0: {x1, y1, x2, y2, ...}
-    const std::vector<double> initial_body_velocities[n * 2]; // The velocities of bodies at time step t=0: {vx1, vy1, vx2, vy2, ...}
-
-    int t = 0; // The current time step, multiply with step_time for current time in secs.
-
-    // The matrix of forces between body i and j.
-    // forces[(i * n + j) * 2] for x, forces[(i * n + j) * 2 + 1] for y
-    // Additionally, forces[(i * n + j) * 2] == forces[(j * n + i) * 2] since matrix is symmetrical
-    double *forces = (double *)malloc(n * n * 2 * sizeof(double));
-
-    // Stores position of all bodies for every time step
-    // all_positions[(i * n + j) * 2 (+1)] to access position_x of body j for timestep i (+1 for position_y)
-    double *all_positions = (double *)malloc(n * total_time_steps * 2 * sizeof(double));
-
-    Body *bodies = (Body *)malloc(n * sizeof(Body));
-
-    // Initialize Body instances
-    for (int i = 0; i < n; i++)
-    {
-        new (&bodies[i]) Body();
-    }
-
-    // std::thread simulation_thread(run_simulation, bodies, n, forces, all_positions, step_time, total_time_steps, threads);
-    // simulation_thread.join();
-
-    auto start = std::chrono::steady_clock::now();
-    sequential_simulation(bodies, n, forces, all_positions, step_time, total_time_steps);
-    auto finish = std::chrono::steady_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(finish - start).count();
-
-    printf("Time elapsed: %d\n", (int)elapsed);
-
-    std::thread simulation_thread([&]() {
-        run_simulation(bodies, forces, distances, n, step_time, step_sync, threads, &bodies_mutex);
-    });
-
-    auto app = Gtk::Application::create(argc, argv, "org.simulation.nbody");
-    Visualizer vis(&bodies, &bodies_mutex);
-    app->run(vis);
-
-    // TODO Concurrent Simulation (call function in main.cpp, DO NOT clog main()!)
-
-    free(forces);
-    free(all_positions);
-    free(bodies);
-    return 0;
-}
-*/
